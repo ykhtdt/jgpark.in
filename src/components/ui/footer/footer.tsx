4fd6b409c45854961e33d658cd467546ce5bc960
jgpark-1 <jgpark7281@gmail.com>
2024-06-07 02:35:55 +0900

    style: remove border from footer in css

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mt-auto text-center text-zinc-400 px-8 py-4 border-t">
+    <footer className="mt-auto text-center text-zinc-400 px-8 py-4">
       <small>By jgpark</small>
     </footer>
   )
