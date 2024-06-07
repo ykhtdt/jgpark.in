ef02ef16ea4ae1826183e4cc3e43a65d294aa76a
jgpark-1 <jgpark7281@gmail.com>
2024-06-08 04:30:37 +0900

    chore: config contentlayer

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withContentlayer } from "next-contentlayer"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -32,4 +34,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withContentlayer(nextConfig)
