6ee0d93b41452fe60d82d806ebf71f208b46134f
jgpark-1 <jgpark7281@gmail.com>
2024-06-07 20:26:40 +0900

    feat: update header links

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -3,68 +3,70 @@
 import Link from "next/link"
 import { useTheme } from "next-themes"
 
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { MoonIcon, SunIcon, GitHubLogoIcon, EnvelopeOpenIcon } from "@radix-ui/react-icons"
 
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown/dropdown-menu"
-
-const navLinks = [
-  {
-    href: "/",
-    label: "Home",
-  },
-  {
-    href: "/posts",
-    label: "Posts",
-  },
-]
 
 const Header = () => {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+
+  const handleThemeChange = () => {
+    setTheme(theme === "light" ? "dark" : "light")
+  }
 
   return (
-    <header className="px-8 py-4 boreder-b">
+    <header className="py-4 boreder-b">
       <div className="flex justify-between items-center w-full h-16">
         <Link href="/" className="font-bold">
           JGPARK
         </Link>
         <nav>
           <ul className="flex items-center gap-x-5 text-sm [&>li]:flex">
-            {navLinks.map((link) => (
-              <li key={link.href}>
-                <Link href={link.href}>
-                  {link.label}
-                </Link>
-              </li>
-            ))}
             <li>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button className="p-0 w-5 h-5 border-none bg-inherit text-foreground shadow-none focus:outline-none focus-visible:ring-0 hover:bg-inherit">
-                    <SunIcon className="h-4 w-h-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <MoonIcon className="absolute h-4 w-h-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">
-                      Toggle theme
-                    </span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Dark
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => setTheme("system")}>
-                    System
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
+              <Button
+                asChild
+                className="p-0 w-5 h-5 border-none bg-inherit text-foreground shadow-none focus:outline-none focus-visible:ring-0 hover:bg-inherit"
+              >
+                <Link
+                  href="mailto:jgpark7281@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <EnvelopeOpenIcon className="w-4 h-4" />
+                  <span className="sr-only">
+                    JGPARK Email
+                  </span>
+                </Link>
+              </Button>
+            </li>
+            <li>
+              <Button
+                asChild
+                className="p-0 w-5 h-5 border-none bg-inherit text-foreground shadow-none focus:outline-none focus-visible:ring-0 hover:bg-inherit"
+              >
+                <Link
+                  href="https://github.com/ykhtdt"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <GitHubLogoIcon className="w-4 h-4" />
+                  <span className="sr-only">
+                    JGPARK Github
+                  </span>
+                </Link>
+              </Button>
+            </li>
+            <li>
+              <Button
+                onClick={handleThemeChange}
+                className="p-0 w-5 h-5 border-none bg-inherit text-foreground shadow-none focus:outline-none focus-visible:ring-0 hover:bg-inherit"
+              >
+                <SunIcon className="w-4 h-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <MoonIcon className="absolute w-4 h-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">
+                  Toggle theme
+                </span>
+              </Button>
             </li>
           </ul>
         </nav>
