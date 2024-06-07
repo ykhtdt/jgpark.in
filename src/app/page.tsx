1816fd24c4ade9047a6af49ae856c9340ce1007b
jgpark-1 <jgpark7281@gmail.com>
2024-06-08 04:56:31 +0900

    feat: update index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import Link from "next/link"
 
+import clsx from "clsx"
+
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/surface/card"
+
 const fetchPinnedRepos = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_APP_URL}/api/github-pinned-repos`,
@@ -18,23 +28,64 @@ const fetchPinnedRepos = async () => {
 }
 
 export default async function Home() {
-  // const repos = await fetchPinnedRepos()
+  const repos = await fetchPinnedRepos()
 
   return (
     <main>
-      <section className="grid items-center md:py-8 py-4 gap-8 pb-10 md:pb-12">
-        <h1 className="text-xl font-bold">
-          Welcome
-        </h1>
-        <section>
+      <section className="grid items-center md:py-8 py-4 gap-9 pb-10 md:pb-12">
+        <section className="flex flex-col gap-4">
+          <h1 className="text-2xl font-bold">
+            Welcome
+          </h1>
           <p className="text-sm leading-8">
-            안녕하세요. 프론트엔드 개발자 <span className="font-bold">박종광</span>입니다.
+            프론트엔드 개발자 <span className="font-bold">박종광</span>
           </p>
         </section>
-        <section>
+        <section className="flex flex-col gap-3">
           <h2 className="text-lg font-semibold">
-            Blog
+            <Link href="/blog">
+              Blog
+            </Link>
           </h2>
+          <p className="text-sm leading-8">
+            기술과 문제를 조금 더 깊게 파보며 과정과 결과를 기록합니다.
+          </p>
+        </section>
+        <section className="flex flex-col gap-4">
+          <h2 className="text-lg font-semibold">
+            <Link
+              href="https://github.com/ykhtdt"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Repository
+            </Link>
+          </h2>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            {repos.slice(0, 4).map((repo) => (
+              <Card key={repo.name} className="bg-card text-card-foreground rounded-none">
+                <CardHeader>
+                  <CardTitle className="font-medium">
+                    {repo.name}
+                  </CardTitle>
+                  <CardDescription>
+                    {repo.description}
+                  </CardDescription>
+                </CardHeader>
+                <CardFooter>
+                  <div className="flex items-center gap-2">
+                    <span className={clsx("inline-block w-3 h-3 border border-muted-foreground rounded-full", {
+                      "bg-yellow-300": repo.language === "Javascript",
+                      "bg-blue-500": repo.language === "Typescript"
+                    })} />
+                    <span className="text-xs text-muted-foreground">
+                      {repo.language}
+                    </span>
+                  </div>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
         </section>
       </section>
     </main>
