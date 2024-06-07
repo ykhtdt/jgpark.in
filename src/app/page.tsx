403cea7fecfefc35569ba0110380b898e5a96096
jgpark-1 <jgpark7281@gmail.com>
2024-06-07 20:25:29 +0900

    feat: update index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,41 @@
-export default function Home() {
+import Link from "next/link"
+
+const fetchPinnedRepos = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/github-pinned-repos`,
+    {
+      next: {
+        revalidate: 300
+      }
+    }
+  )
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch pinned repositories.')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  // const repos = await fetchPinnedRepos()
+
   return (
-    <main className="text-center">
-      <section className="grid items-center md:py-8 container max-w-3xl py-4 gap-12 pb-10 md:pb-12">
-        <h1 className="text-4xl md:text-5xl font-bold">
-        Welcome to my blog
+    <main>
+      <section className="grid items-center md:py-8 py-4 gap-8 pb-10 md:pb-12">
+        <h1 className="text-xl font-bold">
+          Welcome
         </h1>
-        <p className="max-w-[750px] mx-auto leading-8">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam velit quam, rutrum vel massa non, auctor sollicitudin ante. Fusce eleifend quam et neque sollicitudin eleifend. Nulla et nibh sit amet magna commodo elementum a quis quam. Mauris ac massa tortor. Sed a pretium ipsum. Praesent egestas elementum est vitae sollicitudin. Nunc imperdiet tellus ex, eget malesuada ipsum viverra nec.
-        </p>
+        <section>
+          <p className="text-sm leading-8">
+            안녕하세요. 프론트엔드 개발자 <span className="font-bold text-white">박종광</span>입니다.
+          </p>
+        </section>
+        <section>
+          <h2 className="text-lg font-semibold">
+            Blog
+          </h2>
+        </section>
       </section>
     </main>
   )
