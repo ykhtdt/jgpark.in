1559119c6318e31603487316bc70ea943613456e
jgpark-1 <jgpark7281@gmail.com>
2024-06-08 04:31:26 +0900

    feat: change footer text

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="mt-auto text-center text-zinc-400 px-8 py-4">
-      <small>By jgpark</small>
+      <small>&copy; 2024. jgpark blog Powered by Next.js, Vercel</small>
     </footer>
   )
 }
