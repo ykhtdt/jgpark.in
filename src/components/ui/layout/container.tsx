9a9ccbc085434061600e08dd049cdadd67b3b720
jgpark-1 <jgpark7281@gmail.com>
2024-06-07 20:26:08 +0900

    style: update container css

--- src/components/ui/layout/container.tsx
+++ src/components/ui/layout/container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 const Container = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="max-w-4xl mx-auto px-8 min-h-screen flex flex-col">
+    <div className="container px-8 min-h-screen flex flex-col">
       {children}
     </div>
   )
