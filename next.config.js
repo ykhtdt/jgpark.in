c1ca6c90e252c62d92669c812a7ade53ca2f37d3
jgpark-1 <jgpark7281@gmail.com>
2024-06-08 05:16:10 +0900

    chore: update next.config.js extension

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { withContentlayer } from "next-contentlayer"
+const { createContentlayerPlugin } = require("next-contentlayer")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -34,4 +34,8 @@ const nextConfig = {
   },
 }
 
-export default withContentlayer(nextConfig)
+const withContentlayer = createContentlayerPlugin({
+  // Additional Contentlayer config options
+})
+
+module.exports = withContentlayer(nextConfig)
