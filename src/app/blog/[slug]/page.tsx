f67fa74167c44d68efd360233d52583d744f4c19
jg <jgpark7281@gmail.com>
2024-06-08 23:41:30 +0900

    feat: add temp blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -19,17 +19,34 @@ const Page = ({ params }: Props) => {
   //   "coverImage",
   //   "content",
   // ])
-  const post: Post = allPosts.find((post) => post._raw.flattenedPath === params.slug)!
+  const post: Post | undefined = allPosts.find((post) => post._raw.flattenedPath === params.slug)
 
-  console.log(post)
-  console.log(post.body.html)
+  if (!post) {
+    return (
+      <div>
+        Not Found
+      </div>
+    )
+  }
 
   return (
     <main>
-      <div>
-        {post.title}
-      </div>
-      <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      <article className="grid items-center md:py-8 py-4 gap-9 pb-10 md:pb-12">
+        <div className="flex flex-col gap-4">
+          <div className="space-y-2">
+            <time dateTime={post.publishedAt} className="block text-sm text-zinc-500">
+              {format(parseISO(post.publishedAt), "LLLL d, yyyy")}
+            </time>
+            <h1 className="text-3xl font-bold">
+              {post.title}
+            </h1>
+            <p className="text-muted-foreground font-normal">
+              {post.description}
+            </p>
+          </div>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      </article>
     </main>
   )
 }
