c03b0927e9da8a0cc5ce3d1de3daee2150ed6e76
jg <jgpark7281@gmail.com>
2024-06-08 22:21:23 +0900

    chore: integrate contentlayer and outstatic

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,18 @@
-import { defineDocumentType, makeSource } from "contentlayer/source-files"
+import { defineDocumentType, defineNestedType, makeSource } from "contentlayer/source-files"
+
+const Author = defineNestedType(() => ({
+  name: "Category",
+  fields: {
+    name: {
+      type: "string", 
+      required: true
+    },
+    picture: {
+      type: "string", 
+      required: true
+    },
+  },
+}))
 
 export const Post = defineDocumentType(() => ({
   name: "Post",
@@ -8,18 +22,34 @@ export const Post = defineDocumentType(() => ({
       type: "string",
       required: true
     },
-    date: {
-      type: "date",
+    status: {
+      type: "enum",
+      options: ["published", "draft"],
+      required: true
+    },
+    author: {
+      type: "nested",
+      of: Author,
+    },
+    slug: {
+      type: "string",
       required: true
     },
     description: {
       type: "string",
       required: true
     },
+    coverImage: {
+      type: "string",
+    },
+    publishedAt: {
+      type: "date",
+      required: true
+    },
   },
   computedFields: {
-    url: { type: "string", resolve: (post) => `/posts/${post._raw.flattenedPath}` },
+    url: { type: "string", resolve: (post) => `/blog/${post._raw.flattenedPath}` },
   },
 }))
 
-export default makeSource({ contentDirPath: "posts", documentTypes: [Post] })
+export default makeSource({ contentDirPath: "outstatic/content/posts", documentTypes: [Post] })
