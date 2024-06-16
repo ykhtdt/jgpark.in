fdbc2e8e2cc88c22e2002bd5ac31d22140361ddd
jgpark-1 <jgpark7281@gmail.com>
2024-06-16 15:58:10 +0900

    feat: add delay utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const delay = (milliseconds: number) => {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, milliseconds)
+  })
+}
