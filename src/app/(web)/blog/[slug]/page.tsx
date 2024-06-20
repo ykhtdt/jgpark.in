1bb0dd7ac3aa4cbe031f4923e4c91207d6eeb268
jgpark-1 <jgpark7281@gmail.com>
2024-06-21 02:36:19 +0900

    feat: add temp toc

--- src/app/(web)/blog/[slug]/page.tsx
+++ src/app/(web)/blog/[slug]/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link"
 import { notFound } from "next/navigation"
 
 import { getDocumentBySlug, getDocumentSlugs } from "outstatic/server"
 import { format, parseISO } from "date-fns"
 
 import MDXComponents from "@/components/mdx/mdx-components"
+import { generateToc } from "@/lib/mdx"
 
 interface Params {
   params: {
@@ -37,10 +39,37 @@ export async function generateStaticParams() {
 
 const Page = async ({ params }: Params) => {
   const post = await getData(params.slug)
+  const tableOfContent = generateToc(post.content)
 
+  /**
+   * @todo
+   * 현재 스크롤 위치에 따라 TOC를 active 시켜야함
+   */
   return (
-    <main>
-      <div className="grid items-center md:py-8 py-4 gap-9 pb-10 md:pb-12">
+    <main className="relative md:flex md:flex-row">
+      <aside className="fixed order-last hidden translate-x-[864px] w-48 pt-4 md:pt-8 xl:flex">
+        <ol className="w-full text-muted-foreground text-sm space-y-2">
+          {tableOfContent.map((item) => (
+            <li key={item.slug} className="space-y-2">
+              <Link href={`#${item.slug}`} className="transition-colors hover:text-foreground hover:font-medium">
+                {item.text}
+              </Link>
+              {item.children.length > 0 && (
+                <ol className="space-y-2 pl-4">
+                  {item.children.map((child) => (
+                    <li key={child.slug}>
+                      <Link href={`#${child.slug}`} className="transition-colors hover:text-foreground hover:font-medium">
+                        {child.text}
+                      </Link>
+                    </li>
+                  ))}
+                </ol>
+              )}
+            </li>
+          ))}
+        </ol>
+      </aside>
+      <div className="grid items-center pt-4 gap-9 pb-10 md:pt-8 md:pb-12">
         <div className="flex flex-col gap-4">
           <div className="space-y-2">
             <time dateTime={post.publishedAt} className="block text-sm text-zinc-500">
