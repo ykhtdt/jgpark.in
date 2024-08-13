6c9b833785c52a4137e475f89d5ba2f277bb664e
jgpark-1 <jgpark7281@gmail.com>
2024-08-14 00:16:34 +0900

    feat: next.js image blurDataURL generator 정의

---
+++ src/lib/get-image-base64.ts
@@ -0,0 +1,23 @@
+import fetch from "node-fetch"
+import { getPlaiceholder } from "plaiceholder"
+
+export const getImageBase64 = async (src: string) => {
+  const response = await fetch(src)
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch image: ${response.statusText}`)
+  }
+
+  const arrayBuffer = await response.arrayBuffer()
+  const buffer = Buffer.from(arrayBuffer)
+
+  const {
+    metadata: { width, height },
+    ...plaiceholder
+  } = await getPlaiceholder(buffer, { size: 10 })
+
+  return {
+    ...plaiceholder,
+    image: { src, width, height },
+  }
+}
