ce78b64c008f8fa83aefac4aaae38110c16e520f
jgpark-1 <jgpark7281@gmail.com>
2025-03-16 07:36:30 +0900

    fix(apps/web): rehype-pretty-code 버그로 인한 임시 대응

--- apps/web/src/shared/ui/mdx-components.tsx
+++ apps/web/src/shared/ui/mdx-components.tsx
@@ -33,7 +33,22 @@ const components: MDXRemoteProps["components"] = {
 }
 
 const rehypePrettyCodeOptions: RehypePrettyCodeOptions = {
-  theme: "vitesse-dark",
+  /**
+   * 테마를 적용하지 않고 커스텀 테마를 적용하기 위해
+   * theme: "none"을 했었으나 "none"을 지정하는 경우 구문 강조가 사라지고 [!code hl]과 같이 변경된다.
+   * 이에 대한 커스텀 테마에서 구문 강조 등 적용 방법을 현재 파악하지 못해 아래와 같은 임시 방편을 적용한다.
+   * 아래와 같이 작성하면, light/dark 각각 테마가 적용되지 않고 커스텀 테마가 적용되며 구문 강조가 정상 동작한다.
+   * @see https://github.com/rehype-pretty/rehype-pretty-code/issues/248
+   *
+   * 또한, MDXRemote 컴포넌트는 서버 컴포넌트로써 테마를 판단할 수 없기에 CSS로 처리해야한다.
+   * 이는 @workspace/ui/styles/mdx.css에서 적용하고 있다.
+   */
+  // theme: "none",
+  // theme: "vitesse-dark",
+  theme: {
+    dark: "vitesse-dark",
+    light: "vitesse-light",
+  },
   transformers: [transformerNotationDiff(), transformerNotationHighlight()],
 }
 
