87cde18a1457cf9493575d44ceaf554e26e473b3
jgpark-1 <jgpark7281@gmail.com>
2025-03-19 04:19:48 +0900

    feat(apps/web): 블로그 레이아웃 스타일 수정 및 블로그 홈 페이지 적용

--- apps/web/app/blog/page.tsx
+++ apps/web/app/blog/page.tsx
@@ -1,7 +1,61 @@
-import { BlogPostListPage } from "@/pages/blog"
+// import fs from "fs"
+// import path from "path"
+// import matter from "gray-matter"
 
-export default function Page() {
+// import { Post, PostStatus } from "@/entities/blog"
+import { BlogHomePage } from "@/pages/blog"
+
+// const getAllMarkdownFiles = (dir: string): string[] => {
+//   const dirents = fs.readdirSync(dir, { withFileTypes: true })
+
+//   const files = dirents.map(dirent => {
+//     const res = path.join(dir, dirent.name)
+//     return dirent.isDirectory() ? getAllMarkdownFiles(res) : res
+//   })
+
+//   return Array.prototype.concat(...files)
+// }
+
+// const getPostListByStatus = async (status: PostStatus = "published"): Promise<Post[]> => {
+//   const contentDir = path.join(process.cwd(), "content")
+
+//   const allFiles = getAllMarkdownFiles(contentDir)
+
+//   const mdFiles = allFiles
+//     .filter(file => file.endsWith(".md"))
+//     .filter(file => !file.includes(path.join(contentDir, "guide")))
+
+//   const posts = mdFiles
+//     .map(file => {
+//       const fileContent = fs.readFileSync(file, "utf8")
+
+//       const { data } = matter(fileContent)
+
+//       const postStatus = (data.status || "draft") as PostStatus
+
+//       const relativePath = path.relative(contentDir, file)
+//       const slug = relativePath.replace(/\.md$/, "")
+
+//       return {
+//         slug,
+//         frontmatter: {
+//           ...data,
+//           status: postStatus
+//         }
+//       } as Post
+//     })
+//     .filter(post => post.frontmatter.status === status)
+//     .sort((a, b) => {
+//       return new Date(b.frontmatter.publishedAt).getTime() - new Date(a.frontmatter.publishedAt).getTime()
+//     })
+
+//   return posts
+// }
+
+export default async function Page() {
+  // const posts = await getPostListByStatus()
+
   return (
-    <BlogPostListPage />
+    <BlogHomePage />
   )
 }
