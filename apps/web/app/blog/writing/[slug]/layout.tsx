b7b6f70d185a188a9cc4e6bd056460f1786c5f41
jgpark-1 <jgpark7281@gmail.com>
2025-03-19 23:50:27 +0900

    fix(apps/web): 레이아웃에 중첩된 Breadcrumb 컴포넌트 제거

--- apps/web/app/blog/writing/[slug]/layout.tsx
+++ apps/web/app/blog/writing/[slug]/layout.tsx
@@ -1,57 +1,12 @@
-"use client"
-
 import { Fragment } from "react"
-import { usePathname } from "next/navigation"
-
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@workspace/ui/components/breadcrumb"
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const pathname = usePathname()
-
-  const paths = pathname?.split("/").filter(path => path)
-
   return (
     <Fragment>
-      {/* Breadcrumb */}
-      <Breadcrumb>
-        <BreadcrumbList>
-          {paths && paths.map((path, index) => {
-            const isLastItem = index === paths.length - 1
-
-            if (!isLastItem) {
-              return (
-                <Fragment key={path}>
-                  <BreadcrumbItem className="capitalize">
-                    <BreadcrumbLink href={`/${paths.slice(0, index + 1).join("/")}`}>
-                      {path}
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator />
-                </Fragment>
-              )
-            }
-
-            return (
-              <BreadcrumbItem key={path} className="capitalize">
-                <BreadcrumbPage className="capitalize">
-                  {path}
-                </BreadcrumbPage>
-              </BreadcrumbItem>
-            )
-          })}
-        </BreadcrumbList>
-      </Breadcrumb>
 
       {/* page */}
       {children}
