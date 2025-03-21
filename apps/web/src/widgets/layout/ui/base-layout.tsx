e886e8007bd577a605c241f1a9f2b03918a19648
jgpark-1 <jgpark7281@gmail.com>
2025-03-22 03:25:38 +0900

    refactor(apps/web): add classname prop to main content in BaseLayout component

--- apps/web/src/widgets/layout/ui/base-layout.tsx
+++ apps/web/src/widgets/layout/ui/base-layout.tsx
@@ -1,22 +1,27 @@
-import type { ReactNode } from "react"
+import type { PropsWithChildren } from "react"
+
+import { cn } from "@workspace/ui/lib/utils"
 
 import { Container } from "@/shared/ui"
 import { Header } from "@/widgets/header"
 import { Footer } from "@/widgets/footer"
 
 interface BaseLayoutProps {
-  children: ReactNode
+  className?: string
 }
 
-export const BaseLayout = ({ children }: BaseLayoutProps) => {
+export const BaseLayout = ({
+  children,
+  className,
+}: PropsWithChildren<BaseLayoutProps>) => {
   return (
-    <div className="pt-10 sm:pt-20 flex flex-1">
+    <div className="flex flex-1 pt-10 sm:pt-20">
       <Container>
         {/* Header */}
         <Header />
 
         {/* Main content */}
-        <main className="relative flex-1">
+        <main className={cn("relative flex-1", className)}>
           {children}
         </main>
 
