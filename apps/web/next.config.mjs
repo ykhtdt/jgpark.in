7facdb64ee463ffaba75a1b8e95b40eb2c65fc2c
jgpark-1 <jgpark7281@gmail.com>
2025-03-23 04:44:27 +0900

    feat(apps/web): 외부 이미지 사용을 위한 remotePatterns 추가

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cwmkyrrvtewmrirrudnt.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 }
 
 export default nextConfig
