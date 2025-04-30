ad9c7a65d956c5c61eda80d6eab42881317e1fd5
jgpark-1 <jgpark7281@gmail.com>
2025-05-01 00:21:27 +0900

    feat(apps/resume): update resume frontmatter

--- apps/resume/src/widgets/blog-post/ui/frontmatter.tsx
+++ apps/resume/src/widgets/blog-post/ui/frontmatter.tsx
@@ -1,12 +1,51 @@
 import type { PostFrontmatter } from "@/entities/blog"
 
-import { format, parseISO } from "date-fns"
+import {
+  format,
+  parseISO,
+} from "date-fns"
 
 interface FrontmatterProps {
   frontmatter: PostFrontmatter
 }
 
-export const Frontmatter = ({
+export const ResumeFrontmatter = ({
+  frontmatter,
+}: FrontmatterProps) => {
+  console.log(frontmatter)
+  return (
+    <div className="flex justify-between">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-3xl font-bold">
+          {frontmatter.title}
+        </h1>
+        <p className="font-normal text-muted-foreground">
+          {frontmatter.description}
+        </p>
+      </div>
+      <ul className="flex flex-col space-y-px text-sm">
+        <li className="flex items-center gap-2">
+          <span className="font-bold">
+            Email
+          </span>
+          <a href={`mailto:${frontmatter.email}`} className="text-muted-foreground">
+            {frontmatter.email}
+          </a>
+        </li>
+        <li className="flex items-center gap-2">
+          <span className="font-bold">
+            Github
+          </span>
+          <a href={frontmatter.github} target="_blank" rel="noopener noreferrer" className="text-muted-foreground">
+            {frontmatter.github}
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export const BlogPostFrontmatter = ({
   frontmatter,
 }: FrontmatterProps) => {
   return (
@@ -14,7 +53,7 @@ export const Frontmatter = ({
       <time dateTime={frontmatter.publishedAt} className="block text-sm text-zinc-500">
         {format(parseISO(frontmatter.publishedAt), "LLLL d, yyyy")}
       </time>
-      <h1 className="text-xl font-bold">
+      <h1 className="text-2xl font-bold">
         {frontmatter.title}
       </h1>
       <p className="font-normal text-muted-foreground">
