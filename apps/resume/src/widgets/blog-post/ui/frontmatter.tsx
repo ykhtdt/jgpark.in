645fd5796251531e5192763709e0ae4a09412b03
jgpark-1 <jgpark7281@gmail.com>
2025-05-01 01:05:46 +0900

    style(apps/resume): frontmatter 텍스트 컬러 수정

--- apps/resume/src/widgets/blog-post/ui/frontmatter.tsx
+++ apps/resume/src/widgets/blog-post/ui/frontmatter.tsx
@@ -19,7 +19,7 @@ export const ResumeFrontmatter = ({
         <h1 className="text-3xl font-bold">
           {frontmatter.title}
         </h1>
-        <p className="font-normal text-muted-foreground">
+        <p className="font-normal dark:text-muted-foreground">
           {frontmatter.description}
         </p>
       </div>
@@ -28,7 +28,7 @@ export const ResumeFrontmatter = ({
           <span className="font-bold">
             Email
           </span>
-          <a href={`mailto:${frontmatter.email}`} className="text-muted-foreground">
+          <a href={`mailto:${frontmatter.email}`} className="dark:text-muted-foreground">
             {frontmatter.email}
           </a>
         </li>
@@ -36,7 +36,7 @@ export const ResumeFrontmatter = ({
           <span className="font-bold">
             Github
           </span>
-          <a href={frontmatter.github} target="_blank" rel="noopener noreferrer" className="text-muted-foreground">
+          <a href={frontmatter.github} target="_blank" rel="noopener noreferrer" className="dark:text-muted-foreground">
             {frontmatter.github}
           </a>
         </li>
