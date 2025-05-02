70751cb6e69d0b2e159e729e938fa5216eaebb6a
Jonggwang Park <jgpark7281@gmail.com>
2025-05-02 18:36:57 +0900

    chore(apps/resume): 사용하지않는 frontmatter 제거

--- apps/resume/src/widgets/blog-post/index.ts
+++ apps/resume/src/widgets/blog-post/index.ts
@@ -1,6 +1,7 @@
 export {
   ResumeFrontmatter,
-  BlogPostFrontmatter,
 } from "./ui/frontmatter"
 
-export { TableOfContent } from "./ui/table-of-content"
+export {
+  TableOfContent,
+} from "./ui/table-of-content"
